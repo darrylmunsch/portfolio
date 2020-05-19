2756525479348e4e32f76bf6943161d0843163b2
DarrylMunsch <darrylmunsch@gmail.com>
2020-05-19 01:15:00 -0500

    Updates

---
+++ precache-manifest.236a1ad17aa98cd03458d4327f5ee84a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04407677fb9b32d73ea45b068b39abf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f11e6ecf488eb0100087",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "07be056d8d9709365ea2",
+    "url": "/portfolio/static/css/main.92d16cf3.chunk.css"
+  },
+  {
+    "revision": "f11e6ecf488eb0100087",
+    "url": "/portfolio/static/js/2.739f24c3.chunk.js"
+  },
+  {
+    "revision": "40388b5ecbd13d5790bd54ce90f37620",
+    "url": "/portfolio/static/js/2.739f24c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07be056d8d9709365ea2",
+    "url": "/portfolio/static/js/main.cf51e506.chunk.js"
+  },
+  {
+    "revision": "ceb68d410db72963d4f7",
+    "url": "/portfolio/static/js/runtime-main.3fbf267a.js"
+  },
+  {
+    "revision": "6e4d8b4daa9305243cf4c91906b4de9e",
+    "url": "/portfolio/static/media/Resume.6e4d8b4d.pdf"
+  },
+  {
+    "revision": "8fc42e77d46eeb2c414d1e2e174d0f47",
+    "url": "/portfolio/static/media/bgd.8fc42e77.jpg"
+  }
+]);
